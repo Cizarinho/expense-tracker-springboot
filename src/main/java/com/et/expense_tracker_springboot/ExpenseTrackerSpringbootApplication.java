4b98fe806cdf5b2df47a663c1449bb946c41d2a6
Alexander Philipp Geffe <alexandergeffe47@gmail.com>
2026-03-13 10:34:32 +0100

    Erster Commit: Spring Boot Setup

---
+++ src/main/java/com/et/expense_tracker_springboot/ExpenseTrackerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.et.expense_tracker_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/et/expense_tracker_springboot/ExpenseTrackerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.et.expense_tracker_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
